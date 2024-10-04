3547c4aaccfacef7d7f27a11ec6ba3fdfbf342ff
Hammad Hussain <93250@gcslahore.edu.pk>
2024-10-04 17:15:12 +0500

    min heap implementatin

--- max_heap_insertion.cpp
+++ max_heap_insertion.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
-class MaxHeap
+class MinHeap
 {
     int *arr;
     int size;
     int capacity;
 
 public:
-    MaxHeap(int c)
+    MinHeap(int c)
     {
         arr = new int[c];
         size = 0;
@@ -20,14 +20,14 @@ public:
             return;
 
         arr[size++] = val;
-        for (int i = size - 1; i != 0 && arr[(i - 1) / 2] < arr[i];)
+        for (int i = size - 1; i != 0 && arr[(i - 1) / 2] > arr[i];)
         {
             swap(arr[i], arr[(i - 1) / 2]);
             i = (i - 1) / 2;
         }
     }
     // delete from heap
-    void deleteMax()
+    void deleteMin()
     {
         if (size <= 0)
             return; // If heap is empty, do nothing
@@ -38,23 +38,23 @@ public:
 
     void heapify(int i)
     {
-        int largest = i;       // Initialize largest as root
+        int smallest = i;      // Initialize smallest as root
         int left = 2 * i + 1;  // left child
         int right = 2 * i + 2; // right child
 
         // If left child is larger than root
-        if (left < size && arr[left] > arr[largest])
-            largest = left;
+        if (left < size && arr[left] < arr[smallest])
+            smallest = left;
 
-        // If right child is larger than largest so far
-        if (right < size && arr[right] > arr[largest])
-            largest = right;
+        // If right child is larger than smallest so far
+        if (right < size && arr[right] < arr[smallest])
+            smallest = right;
 
-        // If largest is not root
-        if (largest != i)
+        // If smallest is not root
+        if (smallest != i)
         {
-            swap(arr[i], arr[largest]); // Swap root with largest
-            heapify(largest);           // Recursively heapify the affected sub-tree
+            swap(arr[i], arr[smallest]); // Swap root with smallest
+            heapify(smallest);           // Recursively heapify the affected sub-tree
         }
     }
 
@@ -68,7 +68,7 @@ public:
 
 int main()
 {
-    MaxHeap h(10);
+    MinHeap h(10);
     h.insert(10);
     h.insert(20);
     h.insert(15);
@@ -76,7 +76,7 @@ int main()
     h.insert(50);
     h.insert(100);
     h.print();
-    h.deleteMax();
+    h.deleteMin();
     h.print();
 
     return 0;
